125a1809b60e772a95784c459dae49c552a46b09
Aaron <combustablejo@gmail.com>
2017-02-15 11:40:19 -0500

    Static probability model max size.

--- main.cpp
+++ main.cpp
@@ -12,18 +12,11 @@ int main(){
         model.countValue(i);
     }
 
-    cout << "Total Counts: " << model.totalCounts() << endl;
-
-    for(unsigned short i = 0; i < 256; ++i){
-        ProbabilityModel::count_pair p = model.getCount(i);
-        cout << (unsigned char)i << " : " << p.first << ',' << p.second << endl;
-    }
-
     string toEncode = "HOTDOG TO DECODE";
+    model.countValues((unsigned char*)toEncode.c_str(), toEncode.length());
 
     cout << "Encode: '" << toEncode << '\'' << endl;
-    cout << "Original Size: " << toEncode.size()*8 << " bits" << std::endl;
-    encoder.startEncoding();
+    cout << "Original Size: " << toEncode.size() << "x8 = " << toEncode.size()*8 << " bits" << std::endl;
     for(unsigned int i = 0; i < toEncode.length(); ++i){
         encoder.encodeSymbol(toEncode[i]);
     }
@@ -37,13 +30,21 @@ int main(){
     }
     cout << "Number Of Bits: " << encoded_stream_tmp.numberOfBits() << " {";
     bool b = false;
+    unsigned int numberOfZero = 0, numberOfOne = 0;
     while(encoded_stream_tmp.numberOfBits() > 0){
         if(b)   cout << ',';
         else b = true;
-        cout << encoded_stream_tmp.pop_bit();
+        if(encoded_stream_tmp.pop_bit()){
+            cout << 1;
+            numberOfOne++;
+        }else{
+            cout << 0;
+            numberOfZero++;
+        }
     }
-    cout << "}" << endl << endl;
-
+    cout << "}" << endl;
+    cout << "Totals 0:" << numberOfZero << " 1:" << numberOfOne << endl;
+    cout << endl;
 
     cout << "Start Decoding" << endl;
     Decoder<ProbabilityModel> decoder(&model, encoded_stream);
@@ -53,7 +54,42 @@ int main(){
     }
     cout <<  "Finished Decoding" << endl;
 
-    cout << "DONE" << endl;
+    cout << endl << "Test Binary Encoding" << endl;
+
+    BinaryProbabilityModel binaryModel;
+
+    string binaryToEncode = "100000000000000000000100000000000000000000";
+
+    for(unsigned int i = 0; i < binaryToEncode.length(); ++i){
+        binaryModel.countValue(binaryToEncode[i]=='1');
+    }
+
+    cout << "To Encode: " << binaryToEncode << endl;
+    cout << "Length: " << binaryToEncode.length() << endl;
+
+    Encoder<BinaryProbabilityModel> binEncoder(&binaryModel);
+
+    for(unsigned int i = 0; i < binaryToEncode.length(); ++i){
+        binEncoder.encodeSymbol(binaryToEncode[i]=='1');
+    }
+
+    BitStream bin_stream = binEncoder.endEncoding();
+    BitStream bin_stream_tmp = bin_stream;
+
+    cout << endl << "Binary Stream "<< bin_stream_tmp.numberOfBits() <<" {";
+    while(bin_stream_tmp.numberOfBits()){
+        cout << bin_stream_tmp.pop_bit();
+    }
+    cout << "}" << endl;
+
+    cout << "Decoded Binary: ";
+    Decoder<BinaryProbabilityModel> binDecoder(&binaryModel, bin_stream);
+    for(unsigned int i = 0; i < binaryToEncode.length(); ++i){
+        cout <<  binDecoder.decodeSymbol();
+    }
+    cout << endl;
+
+    cout << endl << "DONE" << endl;
     cin.ignore();
     return 0;
 }
